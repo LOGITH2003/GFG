8e63210f8e6fa307dc88a9f3a985a3b320beab59
LOGITH2003 <110965723+LOGITH2003@users.noreply.github.com>
2024-03-20 13:08:13 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/K-th element of two Arrays/kth-element-of-two-arrays.java
@@ -0,0 +1,84 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG {
+	public static void main(String[] args) throws IOException
+	{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine().trim());
+        while(t-->0)
+        {
+            StringTokenizer stt = new StringTokenizer(br.readLine());
+            
+            int n = Integer.parseInt(stt.nextToken());
+            int m = Integer.parseInt(stt.nextToken());
+            int k = Integer.parseInt(stt.nextToken());
+            int a[] = new int[(int)(n)];
+            int b[] = new int[(int)(m)];
+            
+            
+            String inputLine[] = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                a[i] = Integer.parseInt(inputLine[i]);
+            }
+            String inputLine1[] = br.readLine().trim().split(" ");
+            for (int i = 0; i < m; i++) {
+                b[i] = Integer.parseInt(inputLine1[i]);
+            }
+            
+            
+            Solution obj = new Solution();
+            System.out.println(obj.kthElement( a, b, n, m, k));
+            
+        }
+	}
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+class Solution {
+    public long kthElement( int arr1[], int arr2[], int n, int m, int k) {
+        int pos_1 = 0,pos_2 = 0;
+
+        while(pos_1<n && pos_2<m) {
+            // condition 1
+
+            if (arr1[pos_1] < arr2[pos_2]) {
+                if (--k == 0)
+                    return arr1[pos_1];
+                pos_1++;
+            }
+
+            // condition 2
+            else{
+                if (--k == 0)
+                    return arr2[pos_2];
+                pos_2++;
+            }
+        }
+        if(pos_1 == n){
+            while(true){
+                if(--k==0){
+                    return arr2[pos_2];
+                }
+                pos_2++;
+            }
+        }
+        else{
+            while(true){
+                if(--k ==0)
+                    return arr1[pos_1];
+                pos_1++;
+            }
+        }
+        
+    }
+}
