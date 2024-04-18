049190c0901b864b25915491c361591959ac6679
LOGITH2003 <110965723+LOGITH2003@users.noreply.github.com>
2024-04-18 08:37:47 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Two Repeated Elements/two-repeated-elements.java
@@ -0,0 +1,54 @@
+//{ Driver Code Starts
+//Initial template for JAVA
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG
+ {
+	public static void main (String[] args) throws IOException
+	 {
+	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	    int t = Integer.parseInt(br.readLine());
+	    for(int i=0;i<t;i++){
+	        int n = Integer.parseInt(br.readLine());
+	        String l = br.readLine();
+    		String[] sarr = l.split(" ");
+    		int[] arr = new int[sarr.length];
+    		for(int i1=0;i1<arr.length;i1++){
+    			arr[i1] = Integer.parseInt(sarr[i1]);
+    		}
+    		
+            Solution obj = new Solution();
+            
+            int[] res = obj.twoRepeated(arr, n);
+            System.out.println(res[0] + " " + res[1]);
+	    }
+	}
+}
+// } Driver Code Ends
+
+
+//User function template for JAVA
+
+class Solution
+{
+    //Function to find two repeated elements.
+    public int[] twoRepeated(int arr[], int n)
+    {
+        int[] nums = new int[2];
+        HashSet<Integer> set = new HashSet<>();
+        int index = 0;
+        for (int val : arr) {
+            if (!set.add(val)) {
+               nums[index++] = val;
+               if(index == 2)
+                break;
+            }
+        }
+
+        return nums;
+    }
+    
+}
