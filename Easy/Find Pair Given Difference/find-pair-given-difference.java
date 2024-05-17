ba8334d5d1ec55b69c0dc760a8118851cba9407b
LOGITH2003 <110965723+LOGITH2003@users.noreply.github.com>
2024-05-17 17:06:54 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Find Pair Given Difference/find-pair-given-difference.java
@@ -0,0 +1,71 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class IntArray {
+    public static int[] input(BufferedReader br, int n) throws IOException {
+        String[] s = br.readLine().trim().split(" ");
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) a[i] = Integer.parseInt(s[i]);
+
+        return a;
+    }
+
+    public static void print(int[] a) {
+        for (int e : a) System.out.print(e + " ");
+        System.out.println();
+    }
+
+    public static void print(ArrayList<Integer> a) {
+        for (int e : a) System.out.print(e + " ");
+        System.out.println();
+    }
+}
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t;
+        t = Integer.parseInt(br.readLine());
+        while (t-- > 0) {
+
+            int n;
+            n = Integer.parseInt(br.readLine());
+
+            int x;
+            x = Integer.parseInt(br.readLine());
+
+            int[] arr = IntArray.input(br, n);
+
+            Solution obj = new Solution();
+            int res = obj.findPair(n, x, arr);
+
+            System.out.println(res);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+
+class Solution {
+    public int findPair(int n, int x, int[] arr) {
+        ArrayList<Integer> list = new ArrayList<>();
+        for(int i = 0; i < n; i++){
+            if(list.contains(arr[i]))
+                continue;
+            else{
+                list.add(arr[i]);
+                
+                for (int j = i + 1; j < n; j++){
+                    if(x == Math.abs(arr[i] - arr[j])){
+                        return 1;
+                    }
+                }
+            }
+        }
+        
+        return -1;
+    }
+}
